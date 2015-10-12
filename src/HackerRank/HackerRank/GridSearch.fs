25e62c5ac7de2bbbd8c5261f9d167dd05f07e797
Kevin Holditch <kevholditch@hotmail.com>
2015-10-12 22:20:15 +0100

    restructured in a way that i can write a module for each solution

--- src/HackerRank/HackerRank/GridSearch.fs
+++ src/HackerRank/HackerRank/GridSearch.fs
@@ -1,6 +1,9 @@
 ﻿[<AutoOpen>]
 module GridSearch
 
+open System
+open System.IO
+
 let allIndexOf (str:string) (search:string) =
         let rec inner (s:string) l offset =
             match (s.IndexOf(search), (s.IndexOf(search)+1) = s.Length) with
@@ -23,3 +26,25 @@ let gridContains grid (search: string list) =
                          |> Seq.mapi(fun i line -> (i, line))
     matches |> Seq.map(fun (a, b) -> numStrs |> Seq.map(fun (i, line) -> findExact grid line a (b+i+1)))
             |> Seq.exists(fun b -> b |> Seq.forall(fun x -> x))
+
+let solution =
+    let reader = new StreamReader("GridSearch.txt")
+    let read = reader.ReadLine
+    let t = read() |> Convert.ToInt32
+    
+    let num = fun() -> read().Split(' ') 
+                    |> Seq.head
+                    |> Convert.ToInt32
+
+    let fetch = fun() -> [for _ in 1..num() do yield read()]  
+
+    let answers = [1..t] |> Seq.map(fun x -> gridContains (fetch()) (fetch())) |> Seq.toList
+                     
+                     
+    let print b = match b with
+                    | true -> Console.WriteLine("YES")
+                    | false -> Console.WriteLine("NO")                 
+                    
+    answers |> Seq.map(fun a -> print a) |> Seq.toList
+                       
+    reader.Close()
